2f83b8f6ed9d2d6e4419d14406f1a6ca9bff2305
CKATEPTb <taksis@ex.ua>
2020-06-13 00:58:10 +0300

    update

--- script.js
+++ script.js
@@ -6,8 +6,6 @@
             color: '#FFFFFF',
             minDistance: 120,
             maxParticles: 100
-        } );
+        });
     };
-
-    document.querySelector("#bg-music").volume = 0.1; // Changing bg music sound so it will do not scare people
 })();
